08f3633093d03516482e658120c15baaee50260f
mrchiraggarg <gargchirag728@gmail.com>
2025-06-21 00:50:46 +0530

    Changes Modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
   )
 }
 

1c21550320586d82841d503c22938ea9e538551a
mrchiraggarg <gargchirag728@gmail.com>
2025-06-21 12:16:16 +0530

    Changes Modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 // import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Dashboard from './pages/Dashboard'
+import Home from './pages/Home.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
+import Dashboard from './pages/Dashboard.jsx'
 
 export const App = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<div>Home</div>} />
+          <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />

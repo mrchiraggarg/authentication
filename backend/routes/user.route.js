67bf287c779f1ef1a1bc68305a8e04d5da9fedb8
mrchiraggarg <gargchirag728@gmail.com>
2025-06-21 16:15:53 +0530

    Changes Modified

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,8 @@
 import { Router } from "express";
+import { CreateUser } from "../controllers/user.controller";
 
 const router = Router();
 
-router.post('/CreateUser', (req, res) => {
-    const user = req.body;
-    console.log('User data received:', user);
-    res.status(201).json({ message: 'User created successfully', user });
-});
+router.post('/CreateUser', CreateUser);
 
 export default router;

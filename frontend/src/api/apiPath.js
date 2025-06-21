fb24194db6cb65af3b018616beacae47e2367bd8
mrchiraggarg <gargchirag728@gmail.com>
2025-06-21 22:19:09 +0530

    Changes Modified

--- frontend/src/api/apiPath.js
+++ frontend/src/api/apiPath.js
@@ -1,7 +1,7 @@
-export const BASE_URL = process.env.BASE_URL || 'http://localhost:' + (process.env.PORT || 5000);
+export const CORE_URL = 'http://localhost:5000';
 
 export const API_PATHS = {
     USER: {
-        CREATE: `${BASE_URL}/api/user/CreateUser`,
+        CREATE: `${CORE_URL}/api/user/CreateUser`,
     }
 };

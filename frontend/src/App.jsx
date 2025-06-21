2b33b245cfe3be5c9af8403c6bace8774983d20c
mrchiraggarg <gargchirag728@gmail.com>
2025-06-21 12:13:01 +0530

    Changes Modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+// import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
 
 export const App = () => {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Router>
     </>
   )
 }

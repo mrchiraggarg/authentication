65c0a51d21da0d3a0e2a6c4a0ea63069b99d77ff
mrchiraggarg <gargchirag728@gmail.com>
2025-06-21 15:48:33 +0530

    Changes Modified

---
+++ backend/models/user.model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 6,
+        select: false, // Exclude password from queries by default
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+        required: true
+    },
+}, {
+    timestamps: true, // Automatically manage createdAt and updatedAt fields
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;

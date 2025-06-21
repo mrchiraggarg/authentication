da497b698acb26c01cc03a64fd378a1600676426
mrchiraggarg <gargchirag728@gmail.com>
2025-06-21 17:18:15 +0530

    Changes Modified

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    connect()
+    connect(process.env.MONGO_URI)
         .then(() => console.log("MongoDB connected successfully"))
         .catch(err => console.error("MongoDB connection failed:", err));
 });

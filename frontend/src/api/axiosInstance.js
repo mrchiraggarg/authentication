47ee9c156920251a184e7c90ddfbf78a5789f844
mrchiraggarg <gargchirag728@gmail.com>
2025-06-21 22:19:14 +0530

    Changes Modified

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_PATHS } from '../api/apiPath.js'; // Import the API paths from the constants file
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.API_PATHS.USER.CREATE, // Use the API path from the environment variable
+  baseURL: API_PATHS.USER.CREATE, // Use the API path from the environment variable
   timeout: 10000, // Set a timeout of 10 seconds
   headers: {
     'Content-Type': 'application/json',

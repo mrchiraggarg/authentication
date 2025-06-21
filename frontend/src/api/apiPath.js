5c0f9994e12ce78ae15145e5a91f4c6730664580
mrchiraggarg <gargchirag728@gmail.com>
2025-06-21 22:21:15 +0530

    Changes Modified

--- frontend/src/api/apiPath.js
+++ frontend/src/api/apiPath.js
@@ -3,5 +3,6 @@ export const CORE_URL = 'http://localhost:5000';
 export const API_PATHS = {
     USER: {
         CREATE: `${CORE_URL}/api/user/CreateUser`,
+        LOGIN: `${CORE_URL}/api/user/LoginUser`,
     }
 };

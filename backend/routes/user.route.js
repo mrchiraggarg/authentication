10f2e31c7ce17c074e4c8cbb1f0a9a8842f3647b
mrchiraggarg <gargchirag728@gmail.com>
2025-06-22 10:26:24 +0530

    Changes Modified

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { CreateUser, LoginUser } from "../controllers/user.controller.js";
+import { CreateUser, LoginUser, GetAllUsers } from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.post('/CreateUser', CreateUser);
 router.post('/LoginUser', LoginUser);
+router.post('/GetAllUsers', GetAllUsers);
 
 export default router;

395d7a8ce0c59040c95b2d23d4bb03279121d49d
mrchiraggarg <gargchirag728@gmail.com>
2025-06-22 10:26:20 +0530

    Changes Modified

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -97,3 +97,16 @@ export const CreateUser = async (req, res) => {
         });
     }
 }
+
+export const GetAllUsers = async (req, res) => {
+    try {
+        const users = await User.find().select('-password'); // Exclude password from response
+        res.status(200).json(users);
+    } catch (error) {
+        // console.error('Error fetching users:', error);
+        res.status(500).json({ 
+            message: 'Internal server error',
+            error: error.message 
+        });
+    }
+}

4485bc025662b73003ab9b14c6dc893501d23c5a
mrchiraggarg <gargchirag728@gmail.com>
2025-06-25 12:14:48 +0530

    Changes Modified

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 import { API_PATHS } from '../api/apiPath.js'; // Import the API paths from the constants file
 const token = localStorage.getItem('token'); // Get the token from storage
 
+// If the token is not available, it will be undefined
+// If the token is available, it will be a string
+
 const axiosInstance = axios.create({
   baseURL: API_PATHS.USER.CREATE, // Use the API path from the environment variable
   timeout: 10000, // Set a timeout of 10 seconds

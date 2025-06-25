7db898888b2167c62a25ea9845d8e43f1371bd76
mrchiraggarg <gargchirag728@gmail.com>
2025-06-25 22:25:34 +0530

    Changes Modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from './pages/SignIn.jsx'
 import SignUp from './pages/SignUp.jsx'
 import Dashboard from './pages/Dashboard.jsx'
+import EditForm from './pages/EditForm.jsx'
 
 export const App = () => {
   return (
@@ -12,6 +13,7 @@ export const App = () => {
           <Route path="/" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/edit/:id" element={<EditForm />} />
           <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
       </Router>

1f333dc7437b8f16f86fe2ddc0d66e846d483f1e
mrchiraggarg <gargchirag728@gmail.com>
2025-06-25 22:23:40 +0530

    Changes Modified

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axiosInstance from '../api/axiosInstance.js';
 import { API_PATHS } from '../api/apiPath.js';
 

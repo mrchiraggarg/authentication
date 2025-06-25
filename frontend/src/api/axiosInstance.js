5cf1a94ac5df66760f77370b3efc9846e25b8035
mrchiraggarg <gargchirag728@gmail.com>
2025-06-25 11:32:55 +0530

    Changes Modified

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { API_PATHS } from '../api/apiPath.js'; // Import the API paths from the constants file
+const token = localStorage.getItem('token'); // Get the token from storage
 
 const axiosInstance = axios.create({
   baseURL: API_PATHS.USER.CREATE, // Use the API path from the environment variable

f84c8dc509b5cbc007afbf5b4d334b15b7365142
mrchiraggarg <gargchirag728@gmail.com>
2025-06-26 21:44:35 +0530

    Changes Modified

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -7,7 +7,7 @@ const router = Router();
 router.post('/CreateUser', CreateUser);
 router.post('/LoginUser', LoginUser);
 router.post('/GetAllUsers', authenticateToken, GetAllUsers);
-router.get('/GetUserById/:id', authenticateToken, GetUserById);
+router.post('/GetUserById/:id', authenticateToken, GetUserById);
 router.post('/DeleteUser', authenticateToken, DeleteUser);
 
 export default router;
